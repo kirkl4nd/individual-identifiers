b34c7f9e6aea64dc0b23f800ebf13a752f8c4817
kirkl4nd <77247639+kirkl4nd@users.noreply.github.com>
2023-08-04 10:54:35 -0400

    Finalized uniqueness test.

--- src/identifier.rs
+++ src/identifier.rs
@@ -14,6 +14,21 @@ lazy_static! {
     static ref CLIENT: Mutex<Client> = Mutex::new(Client::new());
 }
 
+fn filter_words(words: &[Value], letter: &char, tag: &str) -> Vec<String> {
+    words
+        .iter()
+        .filter_map(|word| {
+            let w = word["word"].as_str()?;
+            let tags = word["tags"].as_array()?;
+            (w.len() >= 4
+                && w.len() <= 8
+                && w.starts_with(*letter)
+                && tags.contains(&Value::String(tag.to_string())))
+            .then(|| w.to_owned())
+        })
+        .collect()
+}
+
 pub enum Identifier {
     Default { uuid: Uuid },
     Success { uuid: Uuid, name: String },
@@ -76,21 +91,6 @@ impl Identifier {
     }
 }
 
-fn filter_words(words: &[Value], letter: &char, tag: &str) -> Vec<String> {
-    words
-        .iter()
-        .filter_map(|word| {
-            let w = word["word"].as_str()?;
-            let tags = word["tags"].as_array()?;
-            (w.len() >= 4
-                && w.len() <= 8
-                && w.starts_with(*letter)
-                && tags.contains(&Value::String(tag.to_string())))
-            .then(|| w.to_owned())
-        })
-        .collect()
-}
-
 impl fmt::Display for Identifier {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
@@ -108,3 +108,95 @@ impl fmt::Display for Identifier {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crossbeam::channel;
+    use std::collections::HashMap;
+    use std::io::{Write, stdout};
+    use std::sync::{Arc, atomic::{AtomicUsize, AtomicBool, Ordering}};
+    use std::thread;
+    use std::time::{Instant, Duration};
+
+    #[test]
+    fn test_uniqueness() {
+        let num_cpus = num_cpus::get(); // get the number of logical cores
+        let pool = rayon::ThreadPoolBuilder::new().num_threads(num_cpus).build().unwrap(); // create a ThreadPool with that many threads
+
+        let (tx, rx) = channel::unbounded();
+        let total = 10000;
+        let start = Arc::new(Instant::now());
+        let counter = Arc::new(AtomicUsize::new(0));
+        let stop = Arc::new(AtomicBool::new(false));
+
+        let counter_clone = Arc::clone(&counter);
+        let start_clone = Arc::clone(&start);
+        let stop_clone = Arc::clone(&stop);
+        thread::spawn(move || {
+            loop {
+                thread::sleep(Duration::from_secs(1));
+                let elapsed = start_clone.elapsed();
+                let count = counter_clone.load(Ordering::SeqCst);
+                print!("\rElapsed time: {:0>2}:{:0>2}, Identifiers created: {}/{}",
+                    elapsed.as_secs() / 60, 
+                    elapsed.as_secs() % 60, 
+                    count, 
+                    total
+                );
+                stdout().flush().unwrap();
+                if count >= total {
+                    stop_clone.store(true, Ordering::SeqCst);
+                    break;
+                } else if elapsed >= Duration::from_secs(60 * 60) {
+                    panic!("Test took too long.");
+                }
+            }
+        });
+
+        let stop_clone = Arc::clone(&stop);
+        // using the thread pool to generate identifiers in parallel
+        pool.scope(|s| {
+            for _ in 0..total {
+                if stop_clone.load(Ordering::SeqCst) {
+                    return;
+                }
+                let tx_clone = tx.clone();
+                let counter_clone = Arc::clone(&counter);  // clone the counter before moving it into the closure
+                s.spawn(move |_| {
+                    let mut identifier = Identifier::new();
+                    identifier.set();
+                    tx_clone.send(identifier).unwrap();
+                    counter_clone.fetch_add(1, Ordering::SeqCst);
+                });
+            }
+        });
+        
+        drop(tx);  // close the channel
+
+        let mut word_counts: HashMap<String, usize> = HashMap::new();
+        let mut pair_counts: HashMap<String, usize> = HashMap::new();
+
+        for identifier in rx {
+            if let Identifier::Success { name, .. } = identifier {
+                let words: Vec<&str> = name.split_whitespace().collect();
+                if words.len() == 2 {
+                    *word_counts.entry(words[0].to_string()).or_insert(0) += 1;
+                    *word_counts.entry(words[1].to_string()).or_insert(0) += 1;
+                    *pair_counts.entry(name).or_insert(0) += 1;
+                }
+            }
+        }
+
+        println!("\n\nRepeated words:");
+        for (word, count) in word_counts.iter().filter(|(_, count)| **count > 1) {
+            println!("{}: {}", word, count);
+        }
+
+        println!("\n\nRepeated pairs:");
+        for (pair, count) in pair_counts.iter().filter(|(_, count)| **count > 1) {
+            println!("{}: {}", pair, count);
+        }
+    }
+}
+
