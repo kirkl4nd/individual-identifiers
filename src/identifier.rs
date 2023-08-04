11eb7777e84510799123d1b53eabe7f343671a6b
kirkl4nd <77247639+kirkl4nd@users.noreply.github.com>
2023-08-04 09:33:23 -0400

    Optimization & removed test from main function.

--- src/identifier.rs
+++ src/identifier.rs
@@ -1,9 +1,18 @@
 use core::fmt;
-
+use lazy_static::lazy_static;
 use rand::seq::SliceRandom;
 use reqwest::blocking::Client;
 use serde_json::Value;
+use std::sync::Mutex;
 use uuid::Uuid;
+
+const ERROR_FETCHING_WORD: &str = "Error fetching word.";
+const ERROR_PARSING_API: &str = "Error parsing API response.";
+const ERROR_API_REQUEST: &str = "Error making API request.";
+
+lazy_static! {
+    static ref CLIENT: Mutex<Client> = Mutex::new(Client::new());
+}
 
 pub enum Identifier {
     Default { uuid: Uuid },
@@ -23,43 +32,15 @@ impl Identifier {
             Identifier::Success { .. } => {}
             Identifier::Default { uuid } | Identifier::Failure { uuid, .. } => {
                 let letter = (rand::random::<u8>() % 26 + b'a') as char;
-
-                let client = Client::new();
-                let response = client
-                    .get(&format!(
-                        "https://api.datamuse.com/words?sp={0}*&&md=p",
-                        letter
-                    ))
-                    .send();
+                let url = format!("https://api.datamuse.com/words?sp={0}*&&md=p", letter);
+                let client = CLIENT.lock().unwrap();
+                let response = client.get(&url).send();
 
                 match response {
                     Ok(res) => match res.json::<Vec<Value>>() {
                         Ok(words) => {
-                            let adjectives: Vec<String> = words
-                                .iter()
-                                .filter_map(|word| {
-                                    let w = word["word"].as_str()?;
-                                    let tags = word["tags"].as_array()?;
-                                    (w.len() >= 4
-                                        && w.len() <= 8
-                                        && w.starts_with(letter)
-                                        && tags.contains(&Value::String("adj".to_string())))
-                                    .then(|| w.to_owned())
-                                })
-                                .collect();
-
-                            let nouns: Vec<String> = words
-                                .iter()
-                                .filter_map(|word| {
-                                    let w = word["word"].as_str()?;
-                                    let tags = word["tags"].as_array()?;
-                                    (w.len() >= 4
-                                        && w.len() <= 8
-                                        && w.starts_with(letter)
-                                        && tags.contains(&Value::String("n".to_string())))
-                                    .then(|| w.to_owned())
-                                })
-                                .collect();
+                            let adjectives = filter_words(&words, &letter, "adj");
+                            let nouns = filter_words(&words, &letter, "n");
 
                             if let (Some(adj), Some(noun)) = (
                                 adjectives.choose(&mut rand::thread_rng()).cloned(),
@@ -72,27 +53,42 @@ impl Identifier {
                             } else {
                                 *self = Identifier::Failure {
                                     uuid: *uuid,
-                                    error: "Error fetching word.".to_string(),
+                                    error: ERROR_FETCHING_WORD.to_string(),
                                 };
                             }
                         }
                         Err(_) => {
                             *self = Identifier::Failure {
                                 uuid: *uuid,
-                                error: "Error parsing API response.".to_string(),
+                                error: ERROR_PARSING_API.to_string(),
                             };
                         }
                     },
                     Err(_) => {
                         *self = Identifier::Failure {
                             uuid: *uuid,
-                            error: "Error making API request.".to_string(),
+                            error: ERROR_API_REQUEST.to_string(),
                         };
                     }
                 }
             }
         }
     }
+}
+
+fn filter_words(words: &[Value], letter: &char, tag: &str) -> Vec<String> {
+    words
+        .iter()
+        .filter_map(|word| {
+            let w = word["word"].as_str()?;
+            let tags = word["tags"].as_array()?;
+            (w.len() >= 4
+                && w.len() <= 8
+                && w.starts_with(*letter)
+                && tags.contains(&Value::String(tag.to_string())))
+            .then(|| w.to_owned())
+        })
+        .collect()
 }
 
 impl fmt::Display for Identifier {
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,5 @@ mod identifier;
 use identifier::Identifier;
 
 fn main() {
-    let mut identifier = Identifier::new();
-    println!("{}", identifier);
-
-    identifier.set();
-    println!("{}", identifier);
+    todo!()
 }
