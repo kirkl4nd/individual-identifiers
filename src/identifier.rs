2f0149e00354cd72518341cc4d38cc4bea77a651
kirkl4nd <77247639+kirkl4nd@users.noreply.github.com>
2023-08-04 09:18:26 -0400

    Implemented Identifier::set

--- src/identifier.rs
+++ src/identifier.rs
@@ -1,6 +1,6 @@
 use core::fmt;
 
-use rand::Rng;
+use rand::{Rng, seq::SliceRandom, thread_rng};
 use reqwest::blocking::Client;
 use serde_json::Value;
 use uuid::Uuid;
@@ -22,48 +22,74 @@ impl Identifier {
         match self {
             Identifier::Success { .. } => {}
             Identifier::Default { uuid } | Identifier::Failure { uuid, .. } => {
-                let letter = (rand::thread_rng().gen::<u8>() % 26 + b'a') as char;
+                let letter = (rand::random::<u8>() % 26 + b'a') as char;
 
                 let client = Client::new();
-                let get_word = |kind: &str| {
-                    client
-                        .get(&format!(
-                            "https://api.datamuse.com/words?sp={0}*&rel_jj{1}={0}",
-                            letter, kind
-                        ))
-                        .send()
-                        .ok()
-                        .and_then(|response| response.json::<Vec<Value>>().ok())
-                        .and_then(|words| {
-                            words
-                                .into_iter()
-                                .find(|word| {
-                                    let w = word["word"].as_str().unwrap_or("");
-                                    w.len() <= 7 && w.starts_with(letter)
-                                })
-                                .map(|word| word["word"].as_str().unwrap_or("").to_owned())
-                        })
-                };
+                let response = client
+                    .get(&format!(
+                        "https://api.datamuse.com/words?sp={0}*&&md=p",
+                        letter
+                    ))
+                    .send();
 
-                match (get_word("b"), get_word("a")) {
-                    (Some(adj), Some(noun)) => {
-                        *self = Identifier::Success {
-                            uuid: *uuid,
-                            name: format!("{} {}", adj.to_uppercase(), noun),
-                        };
-                    }
-                    (err_adj, err_noun) => {
-                        let error_message =
-                            format!("Adjective error: {:?}, Noun error: {:?}", err_adj, err_noun);
+                match response {
+                    Ok(res) => match res.json::<Vec<Value>>() {
+                        Ok(words) => {
+                            let adjectives: Vec<String> = words
+                                .iter()
+                                .filter_map(|word| {
+                                    let w = word["word"].as_str()?;
+                                    let tags = word["tags"].as_array()?;
+                                    (w.len() >= 4 && w.len() <= 8 && w.starts_with(letter) && tags.contains(&Value::String("adj".to_string())))
+                                        .then(|| w.to_owned())
+                                })
+                                .collect();
+
+                            let nouns: Vec<String> = words
+                                .iter()
+                                .filter_map(|word| {
+                                    let w = word["word"].as_str()?;
+                                    let tags = word["tags"].as_array()?;
+                                    (w.len() >= 4 && w.len() <= 8 && w.starts_with(letter) && tags.contains(&Value::String("n".to_string())))
+                                        .then(|| w.to_owned())
+                                })
+                                .collect();
+
+                            if let (Some(adj), Some(noun)) = (
+                                adjectives.choose(&mut rand::thread_rng()).cloned(),
+                                nouns.choose(&mut rand::thread_rng()).cloned(),
+                            ) {
+                                *self = Identifier::Success {
+                                    uuid: *uuid,
+                                    name: format!("{} {}", adj, noun),
+                                };
+                            } else {
+                                *self = Identifier::Failure {
+                                    uuid: *uuid,
+                                    error: "Error fetching word.".to_string(),
+                                };
+                            }
+                        }
+                        Err(_) => {
+                            *self = Identifier::Failure {
+                                uuid: *uuid,
+                                error: "Error parsing API response.".to_string(),
+                            };
+                        }
+                    },
+                    Err(_) => {
                         *self = Identifier::Failure {
                             uuid: *uuid,
-                            error: error_message,
+                            error: "Error making API request.".to_string(),
                         };
                     }
                 }
             }
         }
     }
+
+    
+    
 }
 
 impl fmt::Display for Identifier {
